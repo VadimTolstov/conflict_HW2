230964adfbc9db7f032226f7ca73305bc605cd2a
Вадим <vadimt382@gmail.com>
2023-02-16 16:24:37 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getGreeting()+" что то не так! ");
+        System.out.println(getGreeting()+" Bad");
     }
 
     static String getGreeting() {
-        return "Hello GURU ! ";
+        return "Hello QA ! ";
     }
 }
