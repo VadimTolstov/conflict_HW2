ac8b2daf45e843bcc609b3bec521c13bb969512c
VAdimT <vadimt382@gmail.com>
2023-02-16 16:07:34 +0300

    HW 2.4

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getGreeting()+" что то не так! ");
+    }
+
+    static String getGreeting() {
+        return "Hello GURU ! ";
+    }
+}
