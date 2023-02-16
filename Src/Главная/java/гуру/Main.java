f6b708f09bece6f75ef471c6e8376240de01ca8c
Вадим <vadimt382@gmail.com>
2023-02-16 15:33:26 +0300

    Update and rename src/main/java/guru/Main.java to Src/Главная/java/гуру/Main.java

--- Src/Главная/java/гуру/Main.java
+++ Src/Главная/java/гуру/Main.java
@@ -2,10 +2,10 @@ package guru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getGreeting());
+        System.out.println(getGreeting()+" GURU!");
     }
 
     static String getGreeting(){
-        return "Hello GURU! ";
+        return "Hello CONFLILICT! ";
     }
 }
